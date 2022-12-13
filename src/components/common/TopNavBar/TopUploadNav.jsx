3aaca40e8cf1f9d679cc0a2e49a8fbf2e5a0089f
rosamondkim <itzmems@naver.com>
2022-12-13 15:02:55 +0900

    design: 공통UI 파트2 화면개발 버튼 및 프로필 이미지 컴포넌트 추가 (#21)

--- src/components/common/TopNavBar/TopUploadNav.jsx
+++ src/components/common/TopNavBar/TopUploadNav.jsx
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom';
 
 import { TopNavBar, LeftArrow } from './Styled';
 import { LEFT_ARROW_ICON } from '../../../styles/CommonIcons';
+import Button from '../Button/Button';
 
 const TopUploadNav = () => {
   return (
     <TopNavBar>
       <Link to='/'>
         <LeftArrow src={LEFT_ARROW_ICON} alt='뒤로가기버튼' />
-        {/* Ms--Disabled-button */}
       </Link>
+      <Button size='MS'>저장</Button>
     </TopNavBar>
   );
 };
