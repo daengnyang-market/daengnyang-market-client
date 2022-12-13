cde79585869abde0304d0c1471c5205d220e99f9
rosamondkim <itzmems@naver.com>
2022-12-13 16:43:05 +0900

    design: MainLogo alt내용 수정 및 margin 단위 변경 (#29)

--- src/pages/SplashScreen/SplashScreen.jsx
+++ src/pages/SplashScreen/SplashScreen.jsx
@@ -7,7 +7,7 @@ const SplashScreen = () => {
   return (
     <SplashScreenDiv>
       <SubTitleImg src={SUB_TITLE_IMAGE} alt='우리집 댕냥이를 위한 따뜻한 선물' />
-      <MainLogoImg src={LOGO_IMAGE} alt='가져도댕냥 그림 로고' />
+      <MainLogoImg src={LOGO_IMAGE} alt='가져도댕냥 로고' />
       <MainTitleImg src={MAIN_TITLE_IMAGE} alt='가져도 댕냥 글자 로고' />
     </SplashScreenDiv>
   );
@@ -29,8 +29,8 @@ const SubTitleImg = styled.img`
 
 const MainLogoImg = styled.img`
   width: 160px;
-  margin-top: 54px;
-  margin-bottom: 16.43px;
+  margin-top: 5.4rem;
+  margin-bottom: 1.65rem;
 `;
 
 const MainTitleImg = styled.img`
