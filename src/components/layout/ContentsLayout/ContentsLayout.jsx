9545ce1441edc41d85a337856a1e558ef5c825f1
sypear <bsyeon015@gmail.com>
2022-12-13 23:23:47 +0900

    rename: ContentsLayout으로 파일명 변경 (#47)

--- src/components/layout/ContentsLayout/ContentsLayout.jsx
+++ src/components/layout/ContentsLayout/ContentsLayout.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// * 참고 - children은 <MainLayout></MainLayout> 태그 사이의 내용을 의미합니다. *
+// * 참고 - children은 <ContentsLayout></ContentsLayout> 태그 사이의 내용을 의미합니다. *
 
 // * 사용법 - 1가지 props를 전달해줘야 합니다 *
 // padding : 단축 속성으로 전달해야 합니다. (예: 20px 0 60px 30px, 20px 0 등)
-const MainLayout = ({ children, padding }) => {
-  return <Main padding={padding}>{children}</Main>;
+const ContentsLayout = ({ children, padding }) => {
+  return <ContentsWrapper padding={padding}>{children}</ContentsWrapper>;
 };
 
-export default MainLayout;
+export default ContentsLayout;
 
-const Main = styled.main`
+const ContentsWrapper = styled.main`
   margin-top: 48px;
   margin-bottom: 60px;
   padding: ${(props) => props.padding};
