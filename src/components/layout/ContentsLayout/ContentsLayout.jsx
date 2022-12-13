8bd9088374a24a299c12c28cecf1fa7ad249351d
sypear <bsyeon015@gmail.com>
2022-12-14 08:57:50 +0900

    style: px 단위를 rem으로 변경 (#47)

--- src/components/layout/ContentsLayout/ContentsLayout.jsx
+++ src/components/layout/ContentsLayout/ContentsLayout.jsx
@@ -4,9 +4,9 @@ import styled from 'styled-components';
 // * 참고 - children은 <ContentsLayout></ContentsLayout> 태그 사이의 내용을 의미합니다. *
 
 // * 사용법 - 2가지 props를 전달해줘야 합니다 *
-// isTabMenu : 탭 메뉴 존재 여부를 boolean 값으로 전달해야 합니다. true인 경우 margin-bottom: 60px이 적용됩니다. 기본값은 true입니다. (사용 예: <ContentsLayout isTabMenu={true}>내용</ContentsLayout>))
-// padding : 적용할 패딩을 문자열 값으로 전달해야 합니다. 이때 단축 속성으로 전달해야 합니다. 기본값은 상단 20px, 좌우 16px으로 적용되어 있습니다. (사용 예: <ContentsLayout padding='20px 0 30px 45px'>내용</ContentsLayout>)
-const ContentsLayout = ({ children, isTabMenu = false, padding = '20px 16px 0 16px' }) => {
+// isTabMenu : 탭 메뉴 존재 여부를 boolean 값으로 전달해야 합니다. true인 경우 margin-bottom: 6rem이 적용됩니다. 기본값은 true입니다. (사용 예: <ContentsLayout isTabMenu={true}>내용</ContentsLayout>))
+// padding : 적용할 패딩을 문자열 값으로 전달해야 합니다. 이때 단축 속성으로 전달해야 합니다. 기본값은 상단 2rem, 좌우 1.6rem으로 적용되어 있습니다. (사용 예: <ContentsLayout padding='2rem 0 3rem 4.5rem'>내용</ContentsLayout>)
+const ContentsLayout = ({ children, isTabMenu = false, padding = '2rem 1.6rem 0 1.6rem' }) => {
   return (
     <ContentsWrapper isTabMenu={isTabMenu} padding={padding}>
       {children}
@@ -17,7 +17,7 @@ const ContentsLayout = ({ children, isTabMenu = false, padding = '20px 16px 0 16
 export default ContentsLayout;
 
 const ContentsWrapper = styled.main`
-  margin-top: 48px;
-  margin-bottom: ${(props) => (props.isTabMenu ? '60px' : 0)};
+  margin-top: 4.8rem;
+  margin-bottom: ${(props) => (props.isTabMenu ? '6rem' : 0)};
   padding: ${(props) => props.padding};
 `;
