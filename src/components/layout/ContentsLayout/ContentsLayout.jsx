14e0728d1d12bb6b8c138330d3a5c2310d3577e8
sypear <bsyeon015@gmail.com>
2022-12-14 09:52:22 +0900

    style: isTabMenu 기본값 true로 변경 (#47)

--- src/components/layout/ContentsLayout/ContentsLayout.jsx
+++ src/components/layout/ContentsLayout/ContentsLayout.jsx
@@ -4,9 +4,9 @@ import styled from 'styled-components';
 // * 참고 - children은 <ContentsLayout></ContentsLayout> 태그 사이의 내용을 의미합니다. *
 
 // * 사용법 - 2가지 props를 전달해줘야 합니다 *
-// isTabMenu : 탭 메뉴 존재 여부를 boolean 값으로 전달해야 합니다. true인 경우 margin-bottom: 6rem이 적용됩니다. 기본값은 true입니다. (사용 예: <ContentsLayout isTabMenu={true}>내용</ContentsLayout>))
-// padding : 적용할 패딩을 문자열 값으로 전달해야 합니다. 이때 단축 속성으로 전달해야 합니다. 기본값은 상단 2rem, 좌우 1.6rem으로 적용되어 있습니다. (사용 예: <ContentsLayout padding='2rem 0 3rem 4.5rem'>내용</ContentsLayout>)
-const ContentsLayout = ({ children, isTabMenu = false, padding = '2rem 1.6rem 0 1.6rem' }) => {
+// isTabMenu : 탭 메뉴 존재 여부를 boolean 값으로 전달해야 합니다. true인 경우 margin-bottom: 6rem이 적용됩니다. 기본값은 true입니다.
+// padding : 적용할 패딩을 문자열 값으로 전달해야 합니다. 이때 단축 속성으로 전달해야 합니다. 기본값은 상단 2rem, 좌우 1.6rem으로 적용되어 있습니다.
+const ContentsLayout = ({ children, isTabMenu = true, padding = '2rem 1.6rem 0 1.6rem' }) => {
   return (
     <ContentsWrapper isTabMenu={isTabMenu} padding={padding}>
       {children}
