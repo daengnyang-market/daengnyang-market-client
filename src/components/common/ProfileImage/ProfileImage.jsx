8033a0f83ba0e8074fc29e7477b85b16d123f52a
sypear <bsyeon015@gmail.com>
2022-12-15 10:26:14 +0900

    design: height에 width size 반영, 보더 기본값 0.5로 변경, 커서 옵션 추가 (#93)

--- src/components/common/ProfileImage/ProfileImage.jsx
+++ src/components/common/ProfileImage/ProfileImage.jsx
@@ -5,16 +5,18 @@ import styled from 'styled-components';
 // src : 프로필 이미지 경로
 // alt : 프로필 이미지에 대한 설명 (생략시 기본값: '')
 // width : 프로필 이미지의 width size (단위는 px / 생략시 기본값: 36)
-// borderSize : 프로필 이미지의 테두리 굵기 (단위는 px / 생략시 기본값 : 2)
-const ProfileImage = ({ src, alt = '', width = '36', borderWeight = '2' }) => {
-  return <Image src={src} alt={alt} width={width} borderWeight={borderWeight} />;
+// borderSize : 프로필 이미지의 테두리 굵기 (단위는 px / 생략시 기본값 : 0.5)
+// isPointer : 마우스 커서를 포인터로 할지, 디폴트로 할지 (생략시 기본값 : 포인터로 적용)
+const ProfileImage = ({ src, alt = '', width = '36', borderWeight = '0.5', isPointer = 'true' }) => {
+  return <Image src={src} alt={alt} width={width} borderWeight={borderWeight} isPointer={isPointer} />;
 };
 
 export default ProfileImage;
 
 const Image = styled.img`
   width: ${(props) => props.width}px;
-  height: auto;
+  height: ${(props) => props.width}px;
   border: ${(props) => props.borderWeight}px solid var(--profile-border-color);
   border-radius: 50%;
+  cursor: ${(props) => (props.isPointer ? 'pointer' : 'default')};
 `;
