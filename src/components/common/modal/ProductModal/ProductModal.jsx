3067a57339a6afd8b7dacdb76693aba76a06a60a
sypear <bsyeon015@gmail.com>
2022-12-16 19:37:34 +0900

    design: ProductModal 컴포넌트 구현 (#131)

---
+++ src/components/common/modal/ProductModal/ProductModal.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Alert from '../Alert';
+import ModalLayout from './../ModalLayout';
+import { MenuList, MenuItem } from './../Styled';
+
+const ProductModal = ({ closeModal }) => {
+  const [isOpenAlert, setIsOpenAlert] = useState(false);
+
+  const closeAlert = () => {
+    setIsOpenAlert(false);
+  };
+
+  const deleteProduct = () => {
+    console.log('상품 삭제 로직이 들어갈 위치입니다. 구현시 이 콘솔 로그를 지우고 구현해주세요.');
+    closeModal();
+  };
+
+  return (
+    <>
+      <ModalLayout closeModal={closeModal} isOpenAlert={isOpenAlert}>
+        <MenuList isOpenAlert={isOpenAlert}>
+          <MenuItem>
+            <button type='button' onClick={() => setIsOpenAlert(true)}>
+              삭제
+            </button>
+          </MenuItem>
+          <MenuItem>
+            <Link to='#'>수정</Link>
+          </MenuItem>
+          <MenuItem>
+            <Link to='#'>웹사이트에서 상품 보기</Link>
+          </MenuItem>
+        </MenuList>
+      </ModalLayout>
+      {isOpenAlert ? (
+        <Alert
+          summary='상품 삭제 알림창'
+          title='상품을 삭제하시겠어요?'
+          trigger='삭제'
+          tiggerFunc={deleteProduct}
+          closeAlert={closeAlert}
+        />
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default ProductModal;
