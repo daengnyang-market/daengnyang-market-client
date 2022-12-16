0e85461af069211eb7c4617d3ff88eb8bf7ecb72
sypear <bsyeon015@gmail.com>
2022-12-16 21:10:43 +0900

    design: 판매 상품과 모달 연결 (#136)

--- src/components/carousel/MultiItemCarousel/MultiItemCarousel.jsx
+++ src/components/carousel/MultiItemCarousel/MultiItemCarousel.jsx
@@ -3,8 +3,23 @@ import Product from '../../common/Product/Product';
 import 'swiper/css';
 import { Pagination } from 'swiper';
 import './multiItemCarousel.css';
+import { useState } from 'react';
+import ProductModal from '../../common/modal/ProductModal/ProductModal';
+import { useNavigate } from 'react-router-dom';
 
 export default function App() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isMyProduct, setIsMyProduct] = useState(true); // 모달 테스트용으로 넣어둔 코드입니다. true인 경우에만 모달 출력, false인 경우(다른 사람의 상품인 경우) 상품 링크로 바로 이동되어야 합니다.
+  const navigate = useNavigate();
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+
+  const moveProductUrl = (url) => {
+    window.open(url);
+  };
+
   return (
     <>
       <Swiper
@@ -16,24 +31,55 @@ export default function App() {
         className='mySwiper'
       >
         <SwiperSlide>
-          <Product productImg='https://picsum.photos/250/250' productName='감귤인형' productPrice='30000' />
+          <Product
+            productImg='https://picsum.photos/250/250'
+            productName='감귤인형'
+            productPrice='30000'
+            onClick={() => (isMyProduct ? setIsOpenModal(true) : moveProductUrl('https://www.naver.com'))}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Product productImg='https://picsum.photos/250/250' productName='감귤인형' productPrice='30000' />
+          <Product
+            productImg='https://picsum.photos/250/250'
+            productName='감귤인형'
+            productPrice='30000'
+            onClick={() => (isMyProduct ? setIsOpenModal(true) : moveProductUrl('https://www.naver.com'))}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Product productImg='https://picsum.photos/250/250' productName='감귤인형' productPrice='30000' />
+          <Product
+            productImg='https://picsum.photos/250/250'
+            productName='감귤인형'
+            productPrice='30000'
+            onClick={() => (isMyProduct ? setIsOpenModal(true) : moveProductUrl('https://www.naver.com'))}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Product productImg='https://picsum.photos/250/250' productName='감귤인형' productPrice='30000' />
+          <Product
+            productImg='https://picsum.photos/250/250'
+            productName='감귤인형'
+            productPrice='30000'
+            onClick={() => (isMyProduct ? setIsOpenModal(true) : moveProductUrl('https://www.naver.com'))}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Product productImg='https://picsum.photos/250/250' productName='감귤인형' productPrice='30000' />
+          <Product
+            productImg='https://picsum.photos/250/250'
+            productName='감귤인형'
+            productPrice='30000'
+            onClick={() => (isMyProduct ? setIsOpenModal(true) : moveProductUrl('https://www.naver.com'))}
+          />
         </SwiperSlide>
         <SwiperSlide>
-          <Product productImg='https://picsum.photos/250/250' productName='감귤인형' productPrice='30000' />
+          <Product
+            productImg='https://picsum.photos/250/250'
+            productName='감귤인형'
+            productPrice='30000'
+            onClick={() => (isMyProduct ? setIsOpenModal(true) : moveProductUrl('https://www.naver.com'))}
+          />
         </SwiperSlide>
       </Swiper>
+      {isOpenModal ? <ProductModal closeModal={closeModal} /> : <></>}
     </>
   );
 }
