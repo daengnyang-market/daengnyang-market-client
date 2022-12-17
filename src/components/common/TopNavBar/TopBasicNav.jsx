d9b2ac1840440248c1e19b96741bc1471d08e2eb
sypear <bsyeon015@gmail.com>
2022-12-17 11:05:31 +0900

    feat: pageType에 따라 페이지에 맞는 모달을 보여주는 기능 추가 (#135)

--- src/components/common/TopNavBar/TopBasicNav.jsx
+++ src/components/common/TopNavBar/TopBasicNav.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { TopNavBar, LeftArrow, MoreBtn } from './Styled';
 
 import { LEFT_ARROW_ICON, MORE_ICON } from '../../../styles/CommonIcons';
+import ProfileModal from '../modal/ProfileModal/ProfileModal';
+import ChatRoomModal from '../modal/ChatRoomModal/ChatRoomModal';
 
-const TopBasicNav = () => {
+const TopBasicNav = ({ pageType }) => {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+
   return (
-    <TopNavBar>
-      <Link to='/'>
-        <LeftArrow src={LEFT_ARROW_ICON} alt='뒤로가기 버튼' />
-      </Link>
-      <MoreBtn>
-        <img src={MORE_ICON} alt='더보기 버튼' />
-      </MoreBtn>
-    </TopNavBar>
+    <>
+      <TopNavBar>
+        <Link to='/'>
+          <LeftArrow src={LEFT_ARROW_ICON} alt='뒤로가기 버튼' />
+        </Link>
+        <MoreBtn onClick={() => setIsOpenModal(true)}>
+          <img src={MORE_ICON} alt='더보기 버튼' />
+        </MoreBtn>
+      </TopNavBar>
+      {isOpenModal ? (
+        pageType === 'profile' ? (
+          <ProfileModal closeModal={closeModal} />
+        ) : pageType === 'chatRoom' ? (
+          <ChatRoomModal closeModal={closeModal} />
+        ) : (
+          <></>
+        )
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
 
