f41b6076878fdf22135423311833331cbcefc0ba
rosamondkim <itzmems@naver.com>
2022-12-18 03:59:24 +0900

    feat: ProfilePost 리스트형/앨범형 버튼 (#188)

--- src/pages/profilePage/ProfilePost/ProfilePost.jsx
+++ src/pages/profilePage/ProfilePost/ProfilePost.jsx
@@ -13,39 +13,53 @@ import {
 import { EMPTY_POST_IMAGE } from '../../../styles/CommonImages';
 
 const ProfilePost = ({ postState }) => {
-  const { isExitPost, setIsExitPost } = useState(true);
+  const { isExistPost, setIsExistPost } = useState(postState);
+  // 리스트형 앨범형 전환 버튼
+  const [listBtn, setListBtn] = useState(true);
+  const [albumBtn, setAlbumBtn] = useState(false);
+
+  const handleListBtn = () => {
+    setListBtn(true);
+    setAlbumBtn(false);
+  };
+
+  const handleAlbumBtn = () => {
+    setListBtn(false);
+    setAlbumBtn(true);
+  };
 
   return (
     <>
       <h2 className='sr-only'>게시물 리스트</h2>
       <PostHeader>
-        <ListIcon>
+        <ListIcon onClick={handleListBtn}>
           <strong className='sr-only'>리스트로 보기</strong>
         </ListIcon>
-        <AlbumIcon>
+        <AlbumIcon onClick={handleAlbumBtn}>
           <strong className='sr-only'>앨범으로 보기</strong>
         </AlbumIcon>
       </PostHeader>
-      {/* 리스트버튼 클릭시 리스트형 포스트 목록, 앨범버튼  클릭시 앨범형 포스트목록 나오게 */}
-      {/* <h3 className='sr-only'>리스트형 포스트 목록</h3>
-      <PostUl>
-        <Post userId='@ weniv_Mandarin' userName='애월읍 위니브 감귤농장' />
-        <Post userId='@ weniv_Mandarin' userName='애월읍 위니브 감귤농장' />
-        <Post userId='@ weniv_Mandarin' userName='애월읍 위니브 감귤농장' />
-      </PostUl> */}
-      {/* <h3 className='sr-only'>앨범형 포스트 목록</h3>
-      <PostGrid>
-        <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
-        <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
-        <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
-        <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
-        <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
-        <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
-        <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
-        <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
-        <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
-      </PostGrid> */}
-      {/* postState가 false일 경우 해당 페이지 나오게 */}
+      {listBtn === true ? (
+        <PostUl>
+          <h3 className='sr-only'>리스트형 포스트 목록</h3>
+          <Post userId='@ weniv_Mandarin' userName='애월읍 위니브 감귤농장' />
+          <Post userId='@ weniv_Mandarin' userName='애월읍 위니브 감귤농장' />
+          <Post userId='@ weniv_Mandarin' userName='애월읍 위니브 감귤농장' />
+        </PostUl>
+      ) : (
+        <PostGrid>
+          <h3 className='sr-only'>앨범형 포스트 목록</h3>
+          <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
+          <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
+          <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
+          <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
+          <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
+          <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
+          <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
+          <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
+          <img src='https://picsum.photos/250/250' alt='게시물 썸네일 이미지' />
+        </PostGrid>
+      )}
       <NoPost>
         <img src={EMPTY_POST_IMAGE} alt='포스트가 존재하지 않습니다' />
         <span>아직 작성된 게시글이 없어요.</span>
@@ -63,6 +77,8 @@ const PostHeader = styled.section`
   padding: 0.9em 2em;
   border-bottom: 0.5px solid var(--border-color);
 `;
+
+const PostContents = styled.div``;
 
 const ListIcon = styled.button`
   background: url(${POST_LIST_OFF_ICON}) no-repeat center / 20px;
