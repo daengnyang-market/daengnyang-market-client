1a99226c52aa4032d826efc6bb339ed21c0614f2
sypear <bsyeon015@gmail.com>
2022-12-17 12:31:02 +0900

    feat: 더보기 버튼 클릭 시 ChatRoomModal 출력 (#140)

--- src/components/common/TopNavBar/TopTitleNav.jsx
+++ src/components/common/TopNavBar/TopTitleNav.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 
 import { TopNavBar, LeftArrow, MoreBtn } from './Styled';
 import { LEFT_ARROW_ICON, MORE_ICON } from '../../../styles/CommonIcons';
+import ChatRoomModal from '../modal/ChatRoomModal/ChatRoomModal';
 
 const TopTitleNav = ({ title, viewMoreBtn = true }) => {
   const navigate = useNavigate();
 
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+
   return (
-    <TopNavBar>
-      <button onClick={() => navigate(-1)}>
-        <LeftArrow src={LEFT_ARROW_ICON} alt='뒤로가기 버튼' />
-      </button>
-      <TopNavH2>{title}</TopNavH2>
-      {viewMoreBtn ? (
-        <MoreBtn>
-          <img src={MORE_ICON} alt='더보기 버튼' />
-        </MoreBtn>
-      ) : (
-        <></>
-      )}
-    </TopNavBar>
+    <>
+      <TopNavBar>
+        <button onClick={() => navigate(-1)}>
+          <LeftArrow src={LEFT_ARROW_ICON} alt='뒤로가기 버튼' />
+        </button>
+        <TopNavH2>{title}</TopNavH2>
+        {viewMoreBtn ? (
+          <MoreBtn onClick={() => setIsOpenModal(true)}>
+            <img src={MORE_ICON} alt='더보기 버튼' />
+          </MoreBtn>
+        ) : (
+          <></>
+        )}
+      </TopNavBar>
+      {isOpenModal ? <ChatRoomModal closeModal={closeModal} /> : <></>}
+    </>
   );
 };
 
