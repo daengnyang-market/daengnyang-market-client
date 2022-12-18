220421cc5e3d3d139ae743d8f37d8c029ed8a02e
sypear <bsyeon015@gmail.com>
2022-12-18 17:24:55 +0900

    feat: useInput을 이용해 유효성 검사 적용 (#166)

--- src/pages/EmailLoginPage/EmailLoginPage.jsx
+++ src/pages/EmailLoginPage/EmailLoginPage.jsx
@@ -1,105 +1,88 @@
 import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import useInput from '../../hooks/useInput';
 import Button from '../../components/common/Button/Button';
 
 const EmailLoginPage = () => {
-  const [input, setInput] = useState({ email: '', password: '' });
-  const [isValidLogin, setIsValidLogin] = useState(false);
-  const [isShowAlert, setIsShowAlert] = useState(false);
-
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+  const inputList = [
+    {
+      id: 0,
+      label: '이메일',
+      type: 'text',
+      inputId: 'emailInput',
+      name: 'email',
+      ref: emailInputRef,
+    },
+    {
+      id: 1,
+      label: '비밀번호',
+      type: 'text',
+      inputId: 'passwordInput',
+      name: 'password',
+      ref: passwordInputRef,
+    },
+  ];
+  const [alertMessage, setAlertMessage] = useState({
+    emailAlertMessage: '',
+    passwordAlertMessage: '',
+  });
+  const [inputsValidState, setInputsValidState] = useState({ email: false, password: false });
 
-  const onFocusInputHandler = (e) => {
-    // TODO: 포커스 갔을 때 밑줄 색 바뀌기
-    if (e.currentTarget.id === 'emailInput') {
-      emailInputRef.current.style.borderBottom = '1px solid var(--main-color)';
-      return;
-    }
-
-    passwordInputRef.current.style.borderBottom = '1px solid var(--main-color)';
-  };
-
-  const onBlurInputHandler = (e) => {
-    // TODO: 포커스를 잃었을 때 원 상태로 돌아가기
-    if (e.currentTarget.id === 'emailInput') {
-      emailInputRef.current.style.borderBottom = '1px solid var(--border-color)';
-      return;
-    }
-
-    passwordInputRef.current.style.borderBottom = '1px solid var(--border-color)';
-  };
-
-  const onChangeEmailHandler = (e) => {
-    setInput({ ...input, email: e.currentTarget.value });
-    checkValidLoginState();
-
-    // TODO: 이메일 유효성 검사
-  };
-
-  const onChangePasswordHandler = (e) => {
-    setInput({ ...input, password: e.currentTarget.value });
-    checkValidLoginState();
-
-    // TODO: 비밀번호 유효성 검사
-  };
-
-  const checkValidLoginState = () => {
-    // TODO: 로그인 버튼을 활성화 할 수 있는지 체크
-    if (emailInputRef.current.value && passwordInputRef.current.value) {
-      setIsValidLogin(true);
-      return;
-    }
-
-    setIsValidLogin(false);
-  };
-
-  const onCheckValidHandler = (e) => {
-    // TODO: 로그인 버튼 클릭시
-  };
+  const { values, onBlurHandler, onFocusHandler, onChangeHandler, disabledSubmitButton } = useInput({
+    initailValues: { email: '', password: '' },
+    inputsValidState,
+    setInputsValidState,
+    alertMessage,
+    setAlertMessage,
+  });
 
   return (
     <Main>
       <Title>로그인</Title>
 
-      <LoginInputWrapper>
-        <LoginInput>
-          <LoginInputLabel htmlFor='emailInput'>이메일</LoginInputLabel>
-          <LoginInputText
-            type='text'
-            id='emailInput'
-            ref={emailInputRef}
-            value={input['email']}
-            onFocus={onFocusInputHandler}
-            onBlur={onBlurInputHandler}
-            onChange={onChangeEmailHandler}
-            isShowAlert={isShowAlert}
-            autoComplete='off'
-            spellCheck='false'
-          />
-        </LoginInput>
-        <LoginInput>
-          <LoginInputLabel htmlFor='passwordInput'>비밀번호</LoginInputLabel>
-          <LoginInputText
-            type='text'
-            id='passwordInput'
-            ref={passwordInputRef}
-            value={input['password']}
-            onFocus={onFocusInputHandler}
-            onBlur={onBlurInputHandler}
-            onChange={onChangePasswordHandler}
-            isShowAlert={isShowAlert}
-            autoComplete='off'
-            spellCheck='false'
-          />
-          {isShowAlert ? <LoginInputAlert>이메일 2또는 비밀번호가 일치하지 않습니다.</LoginInputAlert> : <></>}
-        </LoginInput>
-      </LoginInputWrapper>
+      <form>
+        <LoginInputWrapper>
+          {inputList.map(({ id, label, type, inputId, name, ref }) => (
+            <LoginInput key={id}>
+              <LoginInputLabel htmlFor={inputId}>{label}</LoginInputLabel>
+              <LoginInputText
+                type={type}
+                id={inputId}
+                name={name}
+                ref={ref}
+                value={values[name]}
+                onFocus={onFocusHandler}
+                onBlur={onBlurHandler}
+                onChange={onChangeHandler}
+                autoComplete='off'
+                spellCheck='false'
+              />
+              {name === 'email' ? (
+                inputsValidState.email ? (
+                  <></>
+                ) : (
+                  <LoginInputAlert>{alertMessage.emailAlertMessage}</LoginInputAlert>
+                )
+              ) : name === 'password' ? (
+                inputsValidState.password ? (
+                  <></>
+                ) : (
+                  <LoginInputAlert>{alertMessage.passwordAlertMessage}</LoginInputAlert>
+                )
+              ) : (
+                <></>
+              )}
+            </LoginInput>
+          ))}
+        </LoginInputWrapper>
 
-      <Button size='L' disabled={isValidLogin ? false : true} onClick={onCheckValidHandler}>
-        로그인
-      </Button>
+        <Button size='L' disabled={disabledSubmitButton}>
+          로그인
+        </Button>
+      </form>
 
       <JoinLink to='#'>이메일로 회원가입</JoinLink>
     </Main>
@@ -164,8 +147,4 @@ const LoginInputAlert = styled.strong`
   font-size: var(--fs-sm);
   font-weight: 500;
   color: var(--alert-color);
-
-  &::before {
-    content: '* ';
-  }
 `;
