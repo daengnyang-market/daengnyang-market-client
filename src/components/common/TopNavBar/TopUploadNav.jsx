2439bd3e20ddecb3995e895fb2f69f3475abfc7c
euiho.kim <kim-eh@naver.com>
2022-12-20 03:02:24 +0900

    style: 확장성을 위한 코드 추가 (#224)

--- src/components/common/TopNavBar/TopUploadNav.jsx
+++ src/components/common/TopNavBar/TopUploadNav.jsx
@@ -5,7 +5,7 @@ import { TopNavBar, LeftArrow } from './Styled';
 import { LEFT_ARROW_ICON } from '../../../styles/CommonIcons';
 import Button from '../Button/Button';
 
-const TopUploadNav = () => {
+const TopUploadNav = ({ onClick }) => {
   const navigate = useNavigate();
 
   return (
@@ -13,7 +13,9 @@ const TopUploadNav = () => {
       <button onClick={() => navigate(-1)}>
         <LeftArrow src={LEFT_ARROW_ICON} alt='뒤로가기버튼' />
       </button>
-      <Button size='MS'>저장</Button>
+      <Button onClickHandler={onClick} size='MS'>
+        저장
+      </Button>
     </TopNavBar>
   );
 };
