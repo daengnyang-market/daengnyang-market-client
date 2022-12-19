cf304f3b4150810ed458d0a95c92d5d130fd4e46
sypear <bsyeon015@gmail.com>
2022-12-19 23:43:21 +0900

    feat: 로그아웃 기능 구현 (#208)

--- src/components/common/modal/ProfileModal/ProfileModal.jsx
+++ src/components/common/modal/ProfileModal/ProfileModal.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../../context/AuthContext';
 import Alert from '../Alert';
 import ModalLayout from './../ModalLayout';
 import { MenuList, MenuItem } from './../Styled';
 
 const ProfileModal = ({ closeModal }) => {
+  const navigate = useNavigate();
+  const { setToken } = useContext(AuthContext);
   const [isOpenAlert, setIsOpenAlert] = useState(false);
 
   const closeAlert = () => {
@@ -12,8 +15,9 @@ const ProfileModal = ({ closeModal }) => {
   };
 
   const logout = () => {
-    console.log('로그아웃 로직이 들어갈 위치입니다. 구현시 이 콘솔 로그를 지우고 구현해주세요.');
-    closeModal();
+    localStorage.removeItem('token');
+    setToken(null);
+    navigate('/');
   };
 
   return (
