b77a59790857466ce62bf88e79acd7e73242a980
sypear <bsyeon015@gmail.com>
2022-12-20 19:25:57 +0900

    style: Post컴포넌트에 게시글 데이터 전달 및 마크업 수정 (#246)

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -10,10 +10,12 @@ import TabMenu from '../.././components/common/TabMenu/TabMenu';
 import Button from '../../components/common/Button/Button';
 import Post from '../../components/common/Post/Post';
 import { EMPTY_FEED_IMAGE } from '../.././styles/CommonImages';
+import Loading from '../../components/common/Loading/Loading';
 
 const FeedPage = () => {
   const navigate = useNavigate();
   const [isFollowingPost, setIsFollowingPost] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const url = 'https://mandarin.api.weniv.co.kr';
   // const tempToken = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzOWZiYWY0MTdhZTY2NjU4MWM3MzAyMSIsImV4cCI6MTY3NjU5NzIyMSwiaWF0IjoxNjcxNDEzMjIxfQ.H7gXKkMJDOyb0qO3_Zj-aDyFfzIWmVQdeCsyvQ9FEcY`;
@@ -37,10 +39,13 @@ const FeedPage = () => {
 
         await axios(option)
           .then((res) => {
+            setIsLoading(false);
             setIsFollowingPost(res.data.posts);
-            // console.log(res);
           })
-          .catch((err) => console.error(err));
+          .catch((err) => {
+            setIsLoading(false);
+            console.error(err);
+          });
       };
 
       getUserFeed();
@@ -49,43 +54,51 @@ const FeedPage = () => {
 
   return (
     <>
-      <TopMainNav title={'가져도댕냥 피드'} />
-      {isFollowingPost.length > 0 ? (
-        <ContentsLayout>
-          <MainFeedStyle>
-            {isFollowingPost.map((post) => {
-              return (
-                <div key={post.id}>
-                  <Post
-                    userName={post.author.username}
-                    userId={post.author.accountname}
-                    content={post.content}
-                    img={post.image}
-                  />
-                </div>
-              );
-            })}
-          </MainFeedStyle>
-        </ContentsLayout>
+      {isLoading ? (
+        <LoadingWrapper>
+          <Loading />
+        </LoadingWrapper>
       ) : (
-        <EmptyFeedStyle>
-          <ErrorImg src={EMPTY_FEED_IMAGE} alt='에러 이미지' />
-          <span>유저를 검색해 팔로우 해보세요!</span>
-          <Button size='M' onClickHandler={goSearch}>
-            검색하기
-          </Button>
-        </EmptyFeedStyle>
+        <>
+          <TopMainNav title={'가져도댕냥 피드'} />
+          {isFollowingPost.length > 0 ? (
+            <ContentsLayout>
+              <div>
+                {isFollowingPost.map((post) => {
+                  return (
+                    <div key={post.id}>
+                      <Post post={post} />
+                    </div>
+                  );
+                })}
+              </div>
+            </ContentsLayout>
+          ) : (
+            <EmptyFeedStyle>
+              <ErrorImg src={EMPTY_FEED_IMAGE} alt='에러 이미지' />
+              <span>유저를 검색해 팔로우 해보세요!</span>
+              <Button size='M' onClickHandler={goSearch}>
+                검색하기
+              </Button>
+            </EmptyFeedStyle>
+          )}
+          <TabMenu />
+        </>
       )}
-      <TabMenu />
     </>
   );
 };
 
 export default FeedPage;
 
-const MainFeedStyle = styled.main``;
+const LoadingWrapper = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
 
-const EmptyFeedStyle = styled.main`
+const EmptyFeedStyle = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
