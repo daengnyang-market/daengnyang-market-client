94877b009d32a5314c92d5e1fcb3d211cfd70388
sypear <bsyeon015@gmail.com>
2022-12-20 15:13:44 +0900

    style: 필요 없는 div 태그 삭제 (#237)

--- src/components/common/Loading/Loading.jsx
+++ src/components/common/Loading/Loading.jsx
@@ -4,16 +4,12 @@ import { spin } from '../Animation/Animation';
 import { LOADING_ICON } from '../../../styles/CommonIcons';
 
 const Loading = () => {
-  return (
-    <LoadingWrapper>
-      <img src={LOADING_ICON} alt='로딩 중입니다. 잠시만 기다려주세요.' />
-    </LoadingWrapper>
-  );
+  return <LoadingImage src={LOADING_ICON} alt='로딩 중입니다. 잠시만 기다려주세요.' />;
 };
 
 export default Loading;
 
-const LoadingWrapper = styled.div`
+const LoadingImage = styled.img`
   width: 50px;
   height: 50px;
   animation: ${spin} 2000ms linear infinite;
