66ddd457d1c4a39f881dcee8eb94435123cc263a
10_2pang <57481378+yedol1@users.noreply.github.com>
2022-12-20 13:39:02 +0900

    feat: 검색 기능 추가 (#205)

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import TopSearchNav from '../../components/common/TopNavBar/TopSearchNav';
 import ContentsLayout from '../../components/layout/ContentsLayout/ContentsLayout';
 import TabMenu from '../../components/common/TabMenu/TabMenu';
+import axios from 'axios';
+import Search from '../../components/common/userItem/Search/Search';
+import { AuthContextStore } from '../../context/AuthContext';
 const SearchPage = () => {
+  const [searchData, setSearchData] = useState([]);
+  const [keywordData, setKeywordData] = useState('');
+  const { token } = useContext(AuthContextStore);
+
+  const getSearchData = async (searchKeyword) => {
+    await axios({
+      method: 'get',
+      url: `https://mandarin.api.weniv.co.kr/user/searchuser/?keyword=${searchKeyword}`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-type': 'application/json',
+      },
+    }).then((response) => {
+      setSearchData(response.data);
+    });
+  };
+
+  const onTyping = (searchKeyword) => {
+    !searchKeyword ? setSearchData('') : getSearchData(searchKeyword);
+    setKeywordData(searchKeyword);
+  };
+
+  // TODO : 성능개선을 위해, 무한스크롤과 버추얼스크롤을 적용해 볼 예정이다.
   return (
     <ContentsLayout isTabMenu={true} padding='0rem'>
-      <TopSearchNav />
-      <SearchProfileViewer></SearchProfileViewer>
+      <TopSearchNav onTyping={onTyping} />
+      <SearchProfileViewer>
+        {searchData.length === 0 || !searchData ? (
+          <></>
+        ) : (
+          searchData.map((user) => {
+            return (
+              <Search
+                key={Math.random() * 100}
+                keyword={keywordData}
+                image={user.image}
+                accountname={user.accountname}
+                username={user.username}
+              />
+            );
+          })
+        )}
+      </SearchProfileViewer>
       <TabMenu />
     </ContentsLayout>
   );
@@ -25,7 +67,7 @@ const SearchProfileViewer = styled.main`
   height: calc(100% - 10.8rem);
   padding: 2rem 1.6rem;
   overflow-y: scroll;
-  li {
+  div {
     margin-bottom: 1.6rem;
   }
 `;
