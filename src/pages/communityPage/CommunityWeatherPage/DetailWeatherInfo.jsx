aa4221a9681dfcf90acd904407420da508f35e70
sypear <bsyeon015@gmail.com>
2022-12-21 23:28:04 +0900

    feat: 현재 위치를 기반으로 행정구,  날씨, 미세먼지를 출력하는 기능 추가 (#251)

--- src/pages/communityPage/CommunityWeatherPage/DetailWeatherInfo.jsx
+++ src/pages/communityPage/CommunityWeatherPage/DetailWeatherInfo.jsx
@@ -1,32 +1,61 @@
 import React from 'react';
 import styled from 'styled-components';
+import DustBadge from './DustBadgeStyled';
 
-const DetailWeatherInfo = () => {
+const DetailWeatherInfo = ({ weatherInfo, dustInfo }) => {
+  // TODO: 미세먼지 지수 가공
+  const getDustIndex = (dustInfo) => {
+    if (dustInfo <= 30) {
+      return <DustBadge index='good'>좋음 ({dustInfo})</DustBadge>;
+    }
+
+    if (dustInfo <= 50) {
+      return <DustBadge index='notBad'>보통 ({dustInfo})</DustBadge>;
+    }
+
+    if (dustInfo <= 100) {
+      return <DustBadge index='bad'>나쁨 ({dustInfo})</DustBadge>;
+    }
+
+    return <DustBadge index='veryBad'>매우 나쁨 ({dustInfo})</DustBadge>;
+  };
+
   return (
     <DetailInfoWrapper>
       <WeatherList>
         <CurrentTempWrapper>
           <dt className='sr-only'>현재 기온</dt>
           <CurrentTemp>
-            -2<span className='sr-only'>도</span>
+            {weatherInfo.temp}
+            <span className='sr-only'>도</span>
           </CurrentTemp>
         </CurrentTempWrapper>
         <HighLowTempWrapper>
           <dt className='sr-only'>최고/최저 기온</dt>
           <HightTemp aria-label='최고 기온'>
-            6<span className='sr-only'>도</span>
+            {weatherInfo.tempMax}
+            <span className='sr-only'>도</span>
           </HightTemp>
           <LowTemp aria-label='최저 기온'>
-            -4<span className='sr-only'>도</span>
+            {weatherInfo.tempMin}
+            <span className='sr-only'>도</span>
           </LowTemp>
         </HighLowTempWrapper>
+        <WindSpeedWrapper>
+          <dt>풍속</dt>
+          <dd>{weatherInfo.windSpeed} m/s</dd>
+        </WindSpeedWrapper>
+        <HumidityWrapper>
+          <dt>습도</dt>
+          <dd>{weatherInfo.humidity} %</dd>
+        </HumidityWrapper>
         <FineDustWrapper>
           <dt>미세먼지</dt>
-          <dd>좋음</dd>
+          {getDustIndex(dustInfo.pm2_5)}
         </FineDustWrapper>
         <UltraFineDustWrapper>
           <dt>초미세먼지</dt>
-          <dd>좋음</dd>
+          {getDustIndex(dustInfo.pm10)}
         </UltraFineDustWrapper>
       </WeatherList>
     </DetailInfoWrapper>
@@ -42,12 +71,12 @@ const DetailInfoWrapper = styled.div`
 
 const WeatherList = styled.dl`
   display: grid;
-  grid-template-areas: 'currentTemp HighLowTemp' 'FindDust UltraFindDust';
-  grid-row-gap: 3rem;
+  grid-template-areas: 'CurrentTemp HighLowTemp' 'WindSpeed FindDust' 'Humidity UltraFindDust';
+  grid-row-gap: 1.5rem;
 `;
 
 const CurrentTempWrapper = styled.div`
-  grid-area: currentTemp;
+  grid-area: CurrentTemp;
 `;
 
 const CurrentTemp = styled.dd`
@@ -92,13 +121,55 @@ const LowTemp = styled.dd`
   }
 `;
 
-const FineDustWrapper = styled.div`
-  grid-area: FindDust;
+const WindSpeedWrapper = styled.div`
+  grid-area: WindSpeed;
   display: flex;
+  align-items: center;
+  font-size: var(--fs-md);
+  margin-top: 2rem;
+
+  & dt::after {
+    content: '';
+    padding: 0 0.3rem;
+  }
+
+  & dd {
+    background-color: #9d9d9d;
+    color: var(--main-bg-color);
+    border-radius: 3px;
+    padding: 0.5rem;
+  }
+`;
+
+const HumidityWrapper = styled.div`
+  grid-area: Humidity;
+  display: flex;
+  align-items: center;
   font-size: var(--fs-md);
 
   & dt::after {
-    content: ':';
+    content: '';
+    padding: 0 0.3rem;
+  }
+
+  & dd {
+    background-color: #9d9d9d;
+    color: var(--main-bg-color);
+    border-radius: 3px;
+    padding: 0.5rem;
+  }
+`;
+
+const FineDustWrapper = styled.div`
+  grid-area: FindDust;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  font-size: var(--fs-md);
+  margin-top: 2rem;
+
+  & dt::after {
+    content: '';
     padding: 0 0.3rem;
   }
 `;
@@ -107,10 +178,11 @@ const UltraFineDustWrapper = styled.div`
   grid-area: UltraFindDust;
   display: flex;
   justify-content: flex-end;
+  align-items: center;
   font-size: var(--fs-md);
 
   & dt::after {
-    content: ':';
+    content: '';
     padding: 0 0.3rem;
   }
 `;
