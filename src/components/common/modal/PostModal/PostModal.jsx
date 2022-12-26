6d65ae0e135f6b8e2663038c8408d97f6d8e8fd2
rosamondkim <itzmems@naver.com>
2022-12-26 11:29:44 +0900

    :feat: 모달창 게시물 삭제 기능 추가 (#295)

--- src/components/common/modal/PostModal/PostModal.jsx
+++ src/components/common/modal/PostModal/PostModal.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import React, { useState, useContext } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import Alert from '../Alert';
 import ModalLayout from './../ModalLayout';
 import { MenuList, MenuItem } from './../Styled';
+import { AuthContextStore } from '../../../../context/AuthContext';
 
-const PostModal = ({ closeModal, isMyPost }) => {
+const PostModal = ({ closeModal, isMyPost, postID }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { userToken, userAccountname } = useContext(AuthContextStore);
+
   const [isOpenAlert, setIsOpenAlert] = useState(false);
+  const url = `https://mandarin.api.weniv.co.kr`;
 
   const closeAlert = () => {
     setIsOpenAlert(false);
   };
 
   const deletePost = () => {
-    console.log('게시글 삭제 로직이 들어갈 위치입니다. 구현시 이 콘솔 로그를 지우고 구현해주세요.');
+    axios({
+      url: url + `/post/${postID}`,
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+        'Content-type': 'application/json',
+      },
+    })
+      .then((res) => {
+        console.log('삭제완료');
+        window.location.replace('/profile');
+      })
+      .catch((err) => console.error(err));
     closeModal();
   };
 
