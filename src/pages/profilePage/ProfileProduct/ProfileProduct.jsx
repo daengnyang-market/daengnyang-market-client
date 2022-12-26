6a78d8ef7dc0ee8c3489abf6d227d26652212e86
rosamondkim <itzmems@naver.com>
2022-12-27 00:36:04 +0900

    style: 누락된 LoadingWrapper 스타일 추가 (#311)

--- src/pages/profilePage/ProfileProduct/ProfileProduct.jsx
+++ src/pages/profilePage/ProfileProduct/ProfileProduct.jsx
@@ -39,7 +39,9 @@ const ProfileProduct = () => {
   }, [userToken, accountname, productList]);
 
   if (!isRendered) {
-    <Loading />;
+    <LoadingWrapper>
+      <Loading />;
+    </LoadingWrapper>;
   } else {
     return productList.length > 0 ? (
       <>
@@ -56,6 +58,13 @@ const ProfileProduct = () => {
 };
 
 export default ProfileProduct;
+
+const LoadingWrapper = styled.div`
+  position: fixed;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -45%);
+`;
 
 const ProductWrapper = styled.section`
   padding: 2em 0;
