aa7d8f8c9adef0434f3964e0e6250869ff3e09ec
sypear <bsyeon015@gmail.com>
2022-12-26 11:36:08 +0900

    feat: 현재 위치 근처 동물병원 목록 출력 기능 구현 (#279)

--- src/pages/communityPage/communityHospitalPage/CurrentLocationBar.jsx
+++ src/pages/communityPage/communityHospitalPage/CurrentLocationBar.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import styled from 'styled-components';
-import { LOCATION_ICON } from '../../../styles/CommonIcons';
+import React, { useContext } from 'react';
+import styled, { css } from 'styled-components';
+import { UserLocationContextStore } from '../../../context/UserLocationContext';
+import { LOCATION_ICON, UPDATE_WHITE_ICON } from '../../../styles/CommonIcons';
+import { spin } from '../../../components/common/Animation/Animation';
 
-const CurrentLocationBar = () => {
+const CurrentLocationBar = ({ locations = '' }) => {
+  const { district } = useContext(UserLocationContextStore);
+
+  console.log('렌더링');
+
+  const updateLocation = () => {
+    locations.setIsLocationUpdate(true);
+  };
+
   return (
     <CurrentLocationBarWrapper>
-      <CurrentLocation>
-        <span className='sr-only'>현재 위치 : </span>제주도 제주시 무슨동
-      </CurrentLocation>
+      {locations ? (
+        <>
+          <LocationUpdateButton onClick={updateLocation} isLocationUpdate={locations.isLocationUpdate}>
+            <span className='sr-only'>현재 위치 갱신하기</span>
+          </LocationUpdateButton>
+          <CurrentLocation locations={locations}>
+            <span className='sr-only'>현재 위치 : </span>
+            {district}
+          </CurrentLocation>
+        </>
+      ) : (
+        <CurrentLocation locations={locations}>
+          <span className='sr-only'>현재 위치 : </span>
+          {district}
+        </CurrentLocation>
+      )}
     </CurrentLocationBarWrapper>
   );
 };
@@ -15,6 +38,7 @@ const CurrentLocationBar = () => {
 export default CurrentLocationBar;
 
 const CurrentLocationBarWrapper = styled.div`
+  display: flex;
   padding: 1.8rem 3.5rem;
   color: var(--main-bg-color);
   font-size: var(--fs-md);
@@ -24,13 +48,29 @@ const CurrentLocationBarWrapper = styled.div`
 const CurrentLocation = styled.strong`
   display: flex;
   align-items: center;
+  margin-bottom: -0.1rem;
 
-  &::before {
-    display: inline-block;
-    content: '';
-    width: 16px;
-    height: 16px;
-    margin-right: 0.5rem;
-    background: no-repeat center/cover url(${LOCATION_ICON});
-  }
+  ${(props) =>
+    props.locations ||
+    css`
+      &::before {
+        content: '';
+        min-width: 1.5rem;
+        min-height: 1.5rem;
+        margin-right: 0.7rem;
+        background: no-repeat center/contain url(${LOCATION_ICON});
+      }
+    `}
+`;
+
+const LocationUpdateButton = styled.button`
+  min-width: 1.5rem;
+  min-height: 1.5rem;
+  margin-right: 0.7rem;
+  background: no-repeat center/contain url(${UPDATE_WHITE_ICON});
+  animation: ${(props) =>
+    props.isLocationUpdate &&
+    css`
+      ${spin} 2000ms linear infinite
+    `};
 `;
