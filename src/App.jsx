c0beefb0171e888c25d39bfa12698d6cc42e9c04
sypear <bsyeon015@gmail.com>
2022-12-26 09:52:00 +0900

    style: UserLocationContext 추가 (#279)

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import Router from './routes/Router';
 import GlobalStyle from './styles/GlobalStyle';
 import MainLayout from './components/layout/ContentsLayout/MainLayout';
 import AuthContext from './context/AuthContext';
+import UserLocationContext from './context/UserLocationContext';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <AuthContext>
-        <MainLayout>
-          <Router />
-        </MainLayout>
+        <UserLocationContext>
+          <MainLayout>
+            <Router />
+          </MainLayout>
+        </UserLocationContext>
       </AuthContext>
     </>
   );
