bb9c82e007b78348a54229b99eb8f426114cc27e
sypear <bsyeon015@gmail.com>
2022-12-27 17:47:20 +0900

    rename: 오타로 인한 파일명 수정 (#300)

---
+++ src/pages/communityPage/communityHospitalPage/CommunityHospitalMainPage/CommunityHospitaMainPage.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import CommunityLayout from '../../CommunityLayout';
+import HospitalList from './HospitalList';
+import CurrentLocationBar from '../CurrentLocationBar';
+import FilterMenu from './FilterMenu';
+import useCurrentLocation from '../../../../hooks/useCurrentLocation';
+import { BOTTOM_ARROW_ICON } from '../../../../styles/CommonIcons';
+
+const CommunityHospitaMainPage = () => {
+  const [isShowFilterMenu, setIsShowFilterMenu] = useState(false);
+  const [selectFilterId, setSelectFilterId] = useState(0);
+  const filterList = [
+    { id: 0, title: '1km 이내', radius: 1000 },
+    { id: 1, title: '5km 이내', radius: 5000 },
+    { id: 2, title: '10km 이내', radius: 10000 },
+    { id: 3, title: '20km 이내', radius: 20000 },
+  ];
+
+  const [isLocationUpdate, setIsLocationUpdate] = useState(true);
+  const checkUserLocation = useCurrentLocation({ isLocationUpdate, setIsLocationUpdate });
+
+  const onClickBackgroundHandler = () => {
+    setIsShowFilterMenu(!isShowFilterMenu);
+  };
+
+  const onClickFilterHandler = (filterId) => {
+    setSelectFilterId(filterId);
+    onClickBackgroundHandler();
+  };
+
+  return (
+    <CommunityLayout padding='0' currentMenuId={2}>
+      <section>
+        <h2 className='sr-only'>내 근처 동물병원 찾기</h2>
+        <CurrentLocationBar locations={{ isLocationUpdate, setIsLocationUpdate }} />
+
+        <HospitalInfoWrapper>
+          <FilterButton onClick={onClickBackgroundHandler}>{filterList[selectFilterId].title}</FilterButton>
+          {isShowFilterMenu ? (
+            <FilterMenu
+              onBackgroundClick={onClickBackgroundHandler}
+              onClickFilter={onClickFilterHandler}
+              itemList={filterList}
+            />
+          ) : (
+            <></>
+          )}
+          <HospitalList radius={filterList[selectFilterId].radius} />
+        </HospitalInfoWrapper>
+      </section>
+    </CommunityLayout>
+  );
+};
+
+export default CommunityHospitaMainPage;
+
+const HospitalInfoWrapper = styled.div`
+  padding: 3rem 3.5rem;
+`;
+
+const FilterButton = styled.button`
+  display: flex;
+  align-items: center;
+  padding: 0.7rem 1.6rem;
+  margin-bottom: 3rem;
+  font-size: var(--fs-md);
+  border: 1px solid var(--border-color);
+  border-radius: 26px;
+
+  &::after {
+    display: inline-block;
+    content: '';
+    width: 16px;
+    height: 16px;
+    margin-left: 0.6rem;
+    background: no-repeat center/cover url(${BOTTOM_ARROW_ICON});
+  }
+`;
