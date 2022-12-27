c6789c9ab74b4380d56de0957321e881cf8420d9
10_2pang <57481378+yedol1@users.noreply.github.com>
2022-12-27 09:41:14 +0900

    fix: 게시물과 모달 작업시 발생한 자잘한 버그 수정 (#320)

--- src/components/common/modal/PostModal/PostModal.jsx
+++ src/components/common/modal/PostModal/PostModal.jsx
@@ -1,40 +1,31 @@
-
 import axios from 'axios';
 import React, { useState, useContext } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-
+import { Link, useNavigate, useLocation, useParams } from 'react-router-dom';
 import Alert from '../Alert';
 import ModalLayout from './../ModalLayout';
 import { AuthContextStore } from '../../../../context/AuthContext';
 import { MenuList, MenuItem } from './../Styled';
-
 const PostModal = ({ closeModal, isMyPost, postID }) => {
   const location = useLocation();
   const navigate = useNavigate();
   const { userToken } = useContext(AuthContextStore);
-
   const [isOpenAlert, setIsOpenAlert] = useState(false);
   const { postid } = useParams();
-  const navigate = useNavigate();
 
   const [isReport, setIsReport] = useState(false);
   const [isReportSuccess, setIsReportSuccess] = useState(null);
-
   const url = `https://mandarin.api.weniv.co.kr`;
-
   const closeAlert = () => {
     setIsOpenAlert(false);
   };
-
   const handleReportClear = () => {
     setIsReport(false);
     setIsReportSuccess(null);
     closeModal();
   };
-
   const deletePost = () => {
     axios({
-      url: url + `/post/${postID}`,
+      url: url + `/post/${postid}`,
       method: 'DELETE',
       headers: {
         Authorization: `Bearer ${userToken}`,
@@ -46,10 +37,9 @@ const PostModal = ({ closeModal, isMyPost, postID }) => {
       })
       .catch((err) => console.error(err));
   };
-
   const reportPost = () => {
     axios({
-      url: url + `/post/${postID}/report`,
+      url: url + `/post/${postid}/report`,
       method: 'POST',
       headers: {
         Authorization: `Bearer ${userToken}`,
@@ -58,7 +48,6 @@ const PostModal = ({ closeModal, isMyPost, postID }) => {
     })
       .then((res) => {
         setIsReport(true);
-
         if (postID === res.data.report.post) {
           setIsReportSuccess(true);
         } else {
@@ -101,16 +90,6 @@ const PostModal = ({ closeModal, isMyPost, postID }) => {
           trigger={isMyPost ? '삭제' : '신고'}
           tiggerFunc={isMyPost ? deletePost : reportPost}
           closeAlert={closeAlert}
-        />
-      ) : (
-        <></>
-      )}
-      {isReport ? (
-        <Alert
-          summary={isReportSuccess ? '게시글 신고 완료 알림창' : '게시글 신고 실패 알림창'}
-          title={isReportSuccess ? '게시글이 신고되었습니다.' : '게시글 신고에 실패하였습니다.'}
-          trigger={'확인'}
-          tiggerFunc={handleReportClear}
         />
       ) : (
         <></>
