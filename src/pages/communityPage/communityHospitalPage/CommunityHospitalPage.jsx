f2bbaf5dc54cb01e1f05ae9b5c7b6f3e1622528d
sypear <bsyeon015@gmail.com>
2022-12-28 10:10:59 +0900

    style: CommunityHospitalMainPage 파일명 수정 반영 (#300)

--- src/pages/communityPage/communityHospitalPage/CommunityHospitalPage.jsx
+++ src/pages/communityPage/communityHospitalPage/CommunityHospitalPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import CommunityHospitalDetailPage from './CommunityHospitalDetailPage/CommunityHospitalDetailPage';
-import CommunityHospitaMainPage from './CommunityHospitalMainPage/CommunityHospitaMainPage';
+import CommunityHospitalMainPage from './CommunityHospitalMainPage/CommunityHospitalMainPage';
 
 const CommunityHospitalPage = () => {
   const location = useLocation();
 
-  return location.search ? <CommunityHospitalDetailPage /> : <CommunityHospitaMainPage />;
+  return location.search ? <CommunityHospitalDetailPage /> : <CommunityHospitalMainPage />;
 };
 
 export default CommunityHospitalPage;
