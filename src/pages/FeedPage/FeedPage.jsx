9dc77aad8f58f7eebdc15e81bcd30970cefa467f
rosamondkim <itzmems@naver.com>
2022-12-29 00:32:33 +0900

    fix: Feedpage 무한 요청 스크롤 이벤트로 해결 (#354)

--- src/pages/FeedPage/FeedPage.jsx
+++ src/pages/FeedPage/FeedPage.jsx
@@ -16,7 +16,7 @@ import Loading from '../../components/common/Loading/Loading';
 const FeedPage = () => {
   const navigate = useNavigate();
   const [isFollowingPost, setIsFollowingPost] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const url = 'https://mandarin.api.weniv.co.kr';
   const { userToken } = useContext(AuthContextStore);
@@ -30,8 +30,8 @@ const FeedPage = () => {
   const [loading, setLoading] = useState(false);
   const [ref, inView] = useInView();
 
+  // 서버에서 피드를 가져오는 함수
   const getUserFeed = useCallback(async () => {
-    setLoading(true);
     const option = {
       url: url + `/post/feed/?limit=${numFeed}`,
       method: 'GET',
@@ -40,12 +40,14 @@ const FeedPage = () => {
         'Content-type': 'application/json',
       },
     };
+    setLoading(true);
     await axios(option)
       .then((res) => {
         setIsLoading(false);
         setLoading(false);
         setIsFollowingPost(res.data.posts);
-        // console.log('useEffect 확인코드');
+        console.log(isFollowingPost);
+        console.log('useEffect 확인코드 axios');
       })
       .catch((err) => {
         setIsLoading(false);
@@ -53,10 +55,11 @@ const FeedPage = () => {
       });
   }, [numFeed]);
 
+  // getUserFeed 가 바뀔때마다 서버 요청하는 함수 실행
   useEffect(() => {
     if (userToken) {
       getUserFeed();
-      // console.log('useEffect 확인코드2');
+      console.log('useEffect 확인코드 getUserFeed ');
     } else {
       navigate('/');
       return;
@@ -64,11 +67,17 @@ const FeedPage = () => {
   }, [userToken, getUserFeed]);
 
   useEffect(() => {
+    // 사용자가 마지막 요소를 보고있고(inview === true), 로딩중이 아니라면
     if (inView && !loading) {
       setNumFeed((current) => current + 10);
-      // console.log('useEffect 확인코드3');
+      window.scrollTo({ top: 200, behavior: 'smooth' });
     }
   }, [inView, loading]);
+
+  // 진짜 마지막에 다다랐을때 실행해줄 코드를 추가해줄 예정.
+  // 방법을 고민해보자
+  //1. 마지막 게시글 아이디가 새로 불러온 요청의 마지막 게시글 아이디와 똑같다면 axios 호출 멈추기
+  //2. 어차피 스크롤 이벤트니까 띠용효과 주기 . 마지막으로 이동하면 스크롤이 위로 올라가게 => 일단 이렇게 구현함
 
   return (
     <>
@@ -83,6 +92,7 @@ const FeedPage = () => {
             <ContentsLayout>
               <div>
                 {isFollowingPost.map((post, i) =>
+                  // isFollowingPost의 마지막 요소라면 ref추가
                   isFollowingPost.length - 1 === i ? (
                     <div key={post.id} ref={ref} />
                   ) : (
