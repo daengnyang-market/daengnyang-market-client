970871390aa36697e5a051780e88621910f17fef
sypear <bsyeon015@gmail.com>
2022-12-29 17:06:59 +0900

    design: 프로필 페이지 empty 상태에 따라 레이아웃을 다르게 잡는 emptyProfileState 옵션 추가 (#390)

--- src/components/layout/ContentsLayout/ContentsLayout.jsx
+++ src/components/layout/ContentsLayout/ContentsLayout.jsx
@@ -6,9 +6,9 @@ import styled from 'styled-components';
 // * 사용법 - 2가지 props를 전달해줘야 합니다 *
 // isTabMenu : 탭 메뉴 존재 여부를 boolean 값으로 전달해야 합니다. true인 경우 margin-bottom: 6rem이 적용됩니다. 기본값은 true입니다.
 // padding : 적용할 패딩을 문자열 값으로 전달해야 합니다. 이때 단축 속성으로 전달해야 합니다. 기본값은 상단 2rem, 좌우 1.6rem으로 적용되어 있습니다.
-const ContentsLayout = ({ children, isTabMenu = true, padding = '2rem 1.6rem 0 1.6rem' }) => {
+const ContentsLayout = ({ children, isTabMenu = true, padding = '2rem 1.6rem 0 1.6rem', emptyProfileState = null }) => {
   return (
-    <ContentsWrapper isTabMenu={isTabMenu} padding={padding}>
+    <ContentsWrapper isTabMenu={isTabMenu} padding={padding} emptyProfileState={emptyProfileState}>
       {children}
     </ContentsWrapper>
   );
@@ -22,4 +22,7 @@ const ContentsWrapper = styled.main`
   margin-top: 4.8rem;
   margin-bottom: ${(props) => (props.isTabMenu ? '6rem' : 0)};
   padding: ${(props) => props.padding};
+
+  /* 프로필 페이지를 위한 커스텀 */
+  ${(props) => (props.emptyProfileState === 'twice' ? 'height: calc(100vh - 110px);' : 'height: auto;')}
 `;
