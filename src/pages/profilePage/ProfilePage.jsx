54fb413703302875ce1ea078af2ec3c8e480ec65
sypear <bsyeon015@gmail.com>
2022-12-29 16:44:23 +0900

    style: isFill 옵션 삭제 (#390)

--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -66,7 +66,7 @@ const ProfilePage = () => {
       ) : (
         <>
           <TopBasicNav />
-          <ContentsLayout padding='2rem 0 0 0' isFill={emptyPost}>
+          <ContentsLayout padding='2rem 0 0 0'>
             <ProfileHeader profileData={userProfileInfo} profileUserAccountname={accountname} />
             <ProfileProduct />
             <ProfilePost setEmptyPost={setEmptyPost} />
